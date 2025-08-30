0e58ebb22f3e8920ddf5649d38f25352b66147fd
ummfieg <ummfieg@naver.com>
2025-08-30 21:36:03 +0900

    update: weather 이미지 확장자 변경

--- src/utils/weatherRules/weatherMainRules.js
+++ src/utils/weatherRules/weatherMainRules.js
@@ -1,25 +1,61 @@
 const weatherMainRules = [
-  { main: "Clear", message: "맑음", weatherImage: "assets/sunny.png" },
-  { main: "Clouds", message: "구름", weatherImage: "assets/cloud.png" },
-  { main: "Rain", message: "비", weatherImage: "assets/rain.png" },
-  { main: "Drizzle", message: "이슬비", weatherImage: "assets/rain.png" },
-  { main: "Snow", message: "눈", weatherImage: "assets/snow.png" },
+  {
+    main: "Clear",
+    message: "맑음",
+    weatherImage: "assets/weatherImg/sunny.jpeg",
+  },
+  {
+    main: "Clouds",
+    message: "구름",
+    weatherImage: "assets/weatherImg/cloud.jpeg",
+  },
+  { main: "Rain", message: "비", weatherImage: "assets/weatherImg/rain.jpeg" },
+  {
+    main: "Drizzle",
+    message: "이슬비",
+    weatherImage: "assets/weatherImg/rain.jpeg",
+  },
+  { main: "Snow", message: "눈", weatherImage: "assets/weatherImg/snow.jpeg" },
   {
     main: "Thunderstorm",
     message: "천둥⚡️번개",
-    imagweatherImagee: "assets/Thunderstorm.png",
+    imagweatherImagee: "assets/weatherImg/Thunderstorm.jpeg",
   },
-  { main: "Mist", message: "안개", weatherImage: "assets/fog.png" },
-  { main: "Haze", message: "옅은안개", weatherImage: "assets/fog.png" },
-  { main: "Fog", message: "짙은안개", weatherImage: "assets/fog.png" },
-  { main: "Dust", message: "미세먼지", weatherImage: "assets/cloudy.png" },
-  { main: "Sand", message: "황사", weatherImage: "assets/cloudy.png" },
-  { main: "Ash", message: "화산재❗️", weatherImage: "assets/cloudy.png" },
-  { main: "Squall", message: "둘풍❗️", weatherImage: "assets/cloudy.png" },
+  { main: "Mist", message: "안개", weatherImage: "assets/weatherImg/fog.jpeg" },
+  {
+    main: "Haze",
+    message: "옅은안개",
+    weatherImage: "assets/weatherImg/fog.jpeg",
+  },
+  {
+    main: "Fog",
+    message: "짙은안개",
+    weatherImage: "assets/weatherImg/fog.jpeg",
+  },
+  {
+    main: "Dust",
+    message: "미세먼지",
+    weatherImage: "assets/weatherImg/cloudy.jpeg",
+  },
+  {
+    main: "Sand",
+    message: "황사",
+    weatherImage: "assets/weatherImg/cloudy.jpeg",
+  },
+  {
+    main: "Ash",
+    message: "화산재❗️",
+    weatherImage: "assets/weatherImg/cloudy.jpeg",
+  },
+  {
+    main: "Squall",
+    message: "둘풍❗️",
+    weatherImage: "assets/weatherImg/cloudy.jpeg",
+  },
   {
     main: "Tornado",
     message: "토네이도❗️",
-    weatherImage: "assets/Thunderstorm.png",
+    weatherImage: "assets/Thunderstorm.jpeg",
   },
 ];
 
